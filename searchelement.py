0b0de454d9661d374afc7f4978ead9a1e5e2c0d0
Ajjayya <ajjayya2002@gmail.com>
2025-07-08 10:11:36 +0530

    searchelement

---
+++ searchelement.py
@@ -0,0 +1,18 @@
+# mylist = [1,2,3,4,5,6]
+# ele = 100
+# flag = 0
+# for i in mylist:
+#     if(i == ele):
+#         print("element found")
+#         flag = 1
+#         break
+    
+# if (flag == 0):
+#     print("Element not found")
+
+mylist = [1,2,3,4,5,6]
+ele = 59
+if (ele in mylist):
+    print("Element found")
+else:
+    print("Element not found")    
